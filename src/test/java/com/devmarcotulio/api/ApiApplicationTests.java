b768575edb9734e8869f9271e8e2f364f9e191e2
Marco Túlio <marcotulio_iub@hotmail.com>
2024-05-28 13:27:00 -0300

    first commit

---
+++ src/test/java/com/devmarcotulio/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarcotulio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
